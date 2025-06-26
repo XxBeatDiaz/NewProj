a3a68c95fb5d9d244d2d0937cc1b7fd90c9f2a47
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-26 10:26:43 +0300

    question 1

--- mathOps.js
+++
@@ -1,7 +0,0 @@
-const multiply = function(a,b){a*b};
-const divide = function(a,b){a/b};
-
-export{
-    multiply,
-    divide
-}
---
+++ question1/mathOps.js
@@ -0,0 +1,7 @@
+const multiply = function(a,b){return a*b};
+const divide = function(a,b){return a/b};
+
+export{
+    multiply,
+    divide
+}
